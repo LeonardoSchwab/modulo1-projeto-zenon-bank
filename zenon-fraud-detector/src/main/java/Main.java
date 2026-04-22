79ab208b16a5b65b81c9bc8b3ee1cdb16cfad20a
LeonardoSchwab <leodudi@gmail.com>
2026-04-21 23:41:29 -0300

    Ingestão de dados via arquivo

--- zenon-fraud-detector/src/main/java/Main.java
+++ zenon-fraud-detector/src/main/java/Main.java
@@ -1,19 +1,27 @@
 import br.com.zenon.fraud.Transaction;
+import br.com.zenon.fraud.TransactionIngestor;
 
-import java.math.BigDecimal;
-import java.util.List;
+void main() {
+    outputTarefa3();
+}
 
-public class Main {
+public static void outputTarefa2() {
+    List<Transaction> transactions = List.of(new Transaction(1L, Transaction.Payment_type.PAYMENT, new BigDecimal("9839.64"), "C1231006815",
+                    new BigDecimal("170136.0"), new BigDecimal("160296.36"), "M1979787155", new BigDecimal("0.0"),
+                    new BigDecimal("0.0"), 0, 0),
 
-    static void main() {
-        List<Transaction> transactions = List.of(new Transaction(1L, Transaction.Payment_type.PAYMENT, new BigDecimal("9839.64"), "C1231006815",
-                        new BigDecimal("170136.0"), new BigDecimal("160296.36"), "M1979787155", new BigDecimal("0.0"),
-                        new BigDecimal("0.0"), 0, 0),
+            new Transaction(743L, Transaction.Payment_type.CASH_OUT, new BigDecimal("850002.52"), "C1280323807",
+                    new BigDecimal("850002.52"), new BigDecimal("0.0"), "C873221189", new BigDecimal("6510099.11"),
+                    new BigDecimal("7360101.63"), 1, 0));
 
-                new Transaction(743L, Transaction.Payment_type.CASH_OUT, new BigDecimal("850002.52"), "C1280323807",
-                        new BigDecimal("850002.52"), new BigDecimal("0.0"), "C873221189", new BigDecimal("6510099.11"),
-                        new BigDecimal("7360101.63"), 1, 0));
+    transactions.forEach(System.out::println);
+}
 
-        transactions.forEach(System.out::println);
-    }
+public static void outputTarefa3() {
+    TransactionIngestor transactionIngestor = new TransactionIngestor();
+    List<Transaction> transactions = transactionIngestor.getTransactions("data/PS_20174392719_1491204439457_log.csv", 1000);
+
+    IO.println(transactions.size());
+
+    transactions.stream().limit(10).forEach(System.out::println);
 }
---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionIngestor.java
@@ -0,0 +1,58 @@
+package br.com.zenon.fraud;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionIngestor {
+
+    public TransactionIngestor() {
+    }
+
+    /**
+     * Reads a file of transactions with the following structure separated by comma:
+     * step,type,amount,nameOrig,oldbalanceOrg,newbalanceOrig,nameDest,oldbalanceDest,newbalanceDest,isFraud,isFlaggedFraud
+     *
+     * @param filename the file path as String
+     * @param numberOfRowsLimit the number of rows to read
+     * @return A list of transactions from the file
+     * @Author Leonardo Schwab
+     */
+    public List<Transaction> getTransactions(String filename, int numberOfRowsLimit){
+        Path path = Path.of(filename);
+
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        try (BufferedReader bufferedReader = Files.newBufferedReader(path)) {
+
+            int numberOfRows = 0;
+
+            String line = bufferedReader.readLine();
+            if (line.startsWith("step"))
+                line = bufferedReader.readLine();
+            while (line != null && (numberOfRowsLimit == -1 || numberOfRows < numberOfRowsLimit)) {
+                String[] split = line.split(",");
+
+                try {
+                    transactions.add(new Transaction(Long.parseLong(split[0]), Transaction.Payment_type.valueOf(split[1]),
+                            new BigDecimal(split[2]), split[3], new BigDecimal(split[4]), new BigDecimal(split[5]),
+                            split[6], new BigDecimal(split[7]), new BigDecimal(split[8]),
+                            Integer.parseInt(split[9]), Integer.parseInt(split[10])));
+                } catch (IllegalArgumentException e) {
+                    throw new RuntimeException("Incorrect file structure!" + "\r\n" + e);
+                }
+
+                line = bufferedReader.readLine();
+                numberOfRows++;
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return transactions;
+    }
+}
